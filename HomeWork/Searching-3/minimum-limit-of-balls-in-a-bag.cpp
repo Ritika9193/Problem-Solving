da2968b66c260bffe1e6f03c0dc74612d8d489a6
Ritika9193 <118365693+Ritika9193@users.noreply.github.com>
2024-03-12 13:04:15 +0000

    commit

---
+++ HomeWork/Searching-3/minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums, int mid,int maxOperations){
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%mid==0){
+                cnt+=(nums[i]/mid)-1;
+            }
+            else{
+                cnt+=(nums[i]/mid);
+            }
+            if(cnt > maxOperations){
+                return false;
+            }
+        }
+        return true;
+    }
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int start=1;
+        int end=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            end=max(end,nums[i]);
+        }
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(isPossible(nums,mid,maxOperations)){
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+};
